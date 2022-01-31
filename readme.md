0c080dcad6c142185496c35623405048ad7d4117
Rúben Costa <rubenlavoscosta@gmail.com>
2022-01-31 21:42:15 +0000

    Updated Readme

---
+++ readme.md
@@ -0,0 +1,61 @@
+# <center>Express Entry CRS Scrapper</center>
+<center>
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" width="40" height="40"/>
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" width="40" height="40"/>
+</center>
+
+- [Temporary File Cleaner](#-Temporary-file-cleaner)
+    - [Project Explanation](#about-this-project)
+    - [Project Status](#[project-status)
+    - [Configurations](#configurations)
+    - [Technologies](#technologies)
+    - [Help](#sos-help)
+
+
+---
+# <center> ❓ About This Project </center>
+A simple understanding of this project:
+<br />
+I was looking for a way to extract data from a particular [page](https://www.canada.ca/en/immigration-refugees-citizenship/corporate/mandate/policies-operational-instructions-agreements/ministerial-instructions/express-entry-rounds.html#wb-auto-4), Canada's 🍁 Express Entry Draws.
+<br />So I could build a simple Forecast System, but the draws are already at `#215` (At time of writing this ReadMe), which is a lot of data to copy and paste in a short period of time.
+So I came up with this solution **A simple WebScrapper**, which allow us to:
+- Make Screenshots of the pages
+- Export data to a .txt file as well as CSV file or Excel
+- Save time, retrieving data from the website
+
+# <center> ⚒️ Project Status </center>
+Todo: <br />
+
+    Add Docker and Start the Chromium window as Headless
+
+
+# <center> 📜 How to Set Up this utility </center>
+1. Head up to settings.json, this should be the output of the file
+```json
+{
+    "add_screenShots": false,
+    "export_to_excel": false
+}
+```
+    "add_screenShots" -> This is responsible for taking page screenshots
+    "export_to_excel" -> This is responsible to insert data in a Excel Spreadsheet
+2. Enable or Disable the options you consider useful in your case scenario
+3. Navigate to the project directory:
+```bash
+$ cd scrapper # Navigate to project's directory
+$ npm install # This installs all project's dependencies
+$ npm start   # This starts the scrapper
+```
+    Using npm start is going to start scrapping the webpage, a chromium window should pop up, in newer versions the process will be totally headless
+
+
+# <center> Technologies Used </center>
+- [ExcelJs](https://github.com/exceljs/exceljs) 
+- [Puppetteer](https://github.com/puppeteer/puppeteer)
+- [Docker](https://www.docker.com/) [Coming soon in the next versions]
+
+# <center> 🆘 Help! </center>
+
+If you need any help send me an [email](mailto:rubenlavoscosta@gmail.com). Use the subject as: "Express Entry Scrapper"
+
+Made with <3 by Ruben Costa
--- scrapper.js
+++ scrapper.js
@@ -1,20 +1,18 @@
-import puppeteer from "puppeteer";
-import fs from "fs";
-import moment from "moment"
+import puppeteer from "puppeteer"
+import fs from "fs"
 const url ="https://www.canada.ca/en/immigration-refugees-citizenship/corporate/mandate/policies-operational-instructions-agreements/ministerial-instructions/express-entry-rounds.html#wb-auto-4";
 import {writeDataToFile} from './modules/excel.js'
+import config from "./settings.json"
 const dataArray = []
 const dateArray = []
 
-
-
 const accessScore = async (page) => {
-  let dateObj = new Date();
-  let month = dateObj.getUTCMonth() + 1; //months from 1-12
-  let day = dateObj.getUTCDate();
-  let year = dateObj.getUTCFullYear();
+  let dateObj = new Date()
+  let month = dateObj.getUTCMonth() + 1
+  let day = dateObj.getUTCDate()
+  let year = dateObj.getUTCFullYear()
   let time = dateObj.getHours() + "-" + dateObj.getMinutes()
-  let newdate = year + "-" + month + "-" + day;
+  let newdate = year + "-" + month + "-" + day
   let dataHandler = await page.$x("/html/body/main/div[3]/div/div/div[1]/div[2]/text()")
   const entries = await page.evaluate((el) => el.textContent, dataHandler[0]);
   const iteractions = parseInt(entries.split(" ").reverse().splice(1, 1));
@@ -22,19 +20,19 @@ const accessScore = async (page) => {
     for (let j = 1; j <= 25; j++) {
       let elHandle = await page.$x(`/html/body/main/div[3]/div/div/table/tbody/tr[${j}]/td[5]`)
       if(!elHandle) return
-      let score = await page.evaluate((el) => el.textContent, elHandle[0]);
+      let score = await page.evaluate((el) => el.textContent, elHandle[0])
       if(!score) return
       let dateHandle = await page.$x(`/html/body/main/div[3]/div/div/table/tbody/tr[${j}]/td[2]`)
       if(!dateHandle) return
-      let date = await page.evaluate((el) => el.textContent, dateHandle[0]);
+      let date = await page.evaluate((el) => el.textContent, dateHandle[0])
       if(!date) return
       dataArray.push(parseInt(score))
-      const auxiliaryDate = moment(date).format("YYYY-MM-DD")
-      moment(auxiliaryDate).format('YYYY-MM-DD')
-      dateArray.push(auxiliaryDate)
-      await fs.writeFileSync(`./data/results/[${newdate}]-[${time}]-CRS-SCORES.txt`, `${date} -> ${score}\n`, { encoding: "utf8", flag: "a", mode: 0o666 });
+      dateArray.push(date)
+      await fs.writeFileSync(`./data/results/[${newdate}]-[${time}]-CRS-SCORES.txt`, `${date} -> ${score}\n`, { encoding: "utf8", flag: "a", mode: 0o666 })
     }
-    await page.screenshot({ path: `./data/images/[${newdate}]-[${time}]-page-${i}.png`, fullPage: true })
+    if (config.add_screenShots){
+      await page.screenshot({ path: `./data/images/[${newdate}]-[${time}]-page-${i}.png`, fullPage: true })
+    }
     if (i === 5){
       const txtObject = await page.$x(`/html/body/main/div[3]/div/div/div[2]/div/ol/li[7]/a`)
       await txtObject[0].click()
@@ -43,12 +41,9 @@ const accessScore = async (page) => {
       if (!txtObject) return
       await txtObject[0].click()
     }
-    
   }
-
-
+  if (!config.export_to_excel)return
   writeDataToFile("results.xlsx", dateArray, dataArray)
-
 };
 
 const extractPageData = async (url) => {
